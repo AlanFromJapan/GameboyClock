9c9a5fb135ff5c9d5572c6b0062a5c4e75122218
AlanFromJapan <alanfromjapan@gmail.com>
2024-02-15 21:28:13 +0900

    Can send ok IT initial timing is right.
    Need to add a "start" request from GB, or change clock source.

--- utils/GB2Serial/SoftSPI-config.h
+++ utils/GB2Serial/SoftSPI-config.h
@@ -29,9 +29,9 @@
 #define SOFTSPI_PORT 	PORTD
 #define SOFTSPI_DIR		DDRD
 #define SOFTSPI_PIN		PIND
-#define SOFTSPI_CLK		7
-#define SOFTSPI_MOSI	6
-#define SOFTSPI_MISO	5
+#define SOFTSPI_CLK		2
+#define SOFTSPI_MOSI	3
+#define SOFTSPI_MISO	4
 
 //On which clock change to read
 #define SOFTSPI_CLK_READFLAG 	SOFTSPI_CLK_READ_DOWN
@@ -40,14 +40,14 @@
 #define SOFTSPI_BITORDER		SOFTSPI_BITORDER_MSB
 
 //Used PCIEx interrupt (0-1-2 available): IT DEPENDS ON THE PIN YOU USE FOR MOSI.
-//  So read the doc (ie. Atmega328 port D7 = PCINT23 therefore it's PCIE2)
+//  So read the doc (ie. Atmega328 port D2 = PCINT18 therefore it's PCIE2)
 //  To make sure ONLY that pin triggers the interrupt, you have to enable it
 //  and that is done with PCMSKx register
 #define SOFTSPI_PCIE			2
 #define SOFTSPI_IntVect			PCINT2_vect
-//in the mask PCMSK2, the PCINT23 (D7) is the leftmost bit (0x80)
+//in the mask PCMSK2, the PCINT18 (D2) is (0x04)
 #define SOFTSPI_IntPinMask		PCMSK2
-#define SOFTSPI_IntPinMaskBit	7
+#define SOFTSPI_IntPinMaskBit	2
 
 /**
  * ... END
--- utils/GB2Serial/main.c
+++ utils/GB2Serial/main.c
@@ -21,19 +21,11 @@
 
 #include "serialComm.h"
 
-//Soft SPI using default settings (override defines HERE before include command)
-// YOU HAVE TO SET THEM ALL or NONE
-
-////On which clock change to read => GB SENDS on falling edge and READS on rising edge
-//#define SOFTSPI_CLK_READFLAG 	SOFTSPI_CLK_READ_DOWN
-//
-////Expect to receive LSB first (fill byte buffer right to left) or MSB => GB uses MSB
-//#define SOFTSPI_BITORDER		SOFTSPI_BITORDER_MSB
 
 #include "SoftSPI.h"
 
 //test message to send
-const char TEST_MESSAGE[] = "ABCDEFGH";
+const char TEST_MESSAGE[] = "HELLOWOR";
 
 /************************************************************************/
 /* Setup                                                                */
@@ -58,7 +50,7 @@ void mainSetup() {
  * Testing function for RECEPTION on the SPI and echo send to Serial UART (PC)
  *
  */
-inline void test_SoftSPI_reception() {
+inline static void test_SoftSPI_reception() {
 	uint8_t val = 0;
 	while (softspi_hasData()) {
 		val = softspi_getByte();
@@ -79,8 +71,14 @@ inline void test_SoftSPI_reception() {
  *
  */
 static inline void test_SoftSPI_emission(const char c) {
-	USART_Transmit(c);
-	USART_SendString(" ");
+	char vBuff[5];
+	itoa(c, vBuff, 16);
+	USART_SendString("0x");
+	USART_SendString(vBuff);
+	USART_SendString("(");
+	itoa(c, vBuff, 10);
+	USART_SendString(vBuff);
+	USART_SendString(") ");
 
 	while (!softspi_bytesent()){
 		;
@@ -88,7 +86,7 @@ static inline void test_SoftSPI_emission(const char c) {
 	//road is clear
 	softspi_sendByte(c);
 
-	_delay_ms(200);
+	_delay_ms(20);
 }
 
 
@@ -104,17 +102,29 @@ int main(void) {
 	USART_SendString("Let's start.\n");
 
 
+	// ******************* Testing EMISSION one byte at a time *******************
+	USART_SendString("Testing EMISSION byte per byte\n");
+	sei();
+
+	while (1) {
+		for (uint8_t i = 0; i < strlen(TEST_MESSAGE); i++){
+			test_SoftSPI_emission(TEST_MESSAGE[i] - 'A' );
+			//test_SoftSPI_emission(i+1);
+		}
+	}
+/*
 	// ******************* Testing EMISSION *******************
 	USART_SendString("Testing EMISSION (from muC to GB + echo to Serial UART).\n");
 	sei();
 
 	while (1) {
 		for (uint8_t i = 0; i < strlen(TEST_MESSAGE); i++){
-			test_SoftSPI_emission(TEST_MESSAGE[i]);
+			//test_SoftSPI_emission(TEST_MESSAGE[i]);
+			test_SoftSPI_emission(i+1);
 		}
 		_delay_ms(1000);
 	}
-
+*/
 /*
 	// ******************* Testing RECEPTION *******************
 	USART_SendString("Testing RECEPTION (from GB to muC + echo to Serial UART).\n");
