1f2a07ca433bdfc5b9bb36411c580e271ae3062c
AlanFromJapan <alanfromjapan@gmail.com>
2024-07-19 21:35:18 +0900

    It builds! Not tested yet.

--- LinkMini/GBLinkMini-Test/RTC/RTC-shared.c
+++ LinkMini/GBLinkMini-Test/RTC/RTC-shared.c
@@ -12,9 +12,6 @@
 
 #include "RTC-shared.h"
 
-//for sprintf()
-#include <stdio.h>
-
 
 // Convert normal decimal numbers to binary coded decimal
 uint8_t decToBcd(uint8_t val)
@@ -28,14 +25,6 @@ uint8_t bcdToDec(uint8_t val)
   return ( (val/16*10) + (val%16) );
 }
 
-
-/*********************************************************************/
-/* Returns a string representation of the date                       */
-/*********************************************************************/
-void dateToString (char *buffer, Date *d){
-	//sprintf(buffer, "%00d:%00d:%00d", d->hour, d->minute, d->second);
-	sprintf(buffer, "%02d/%02d/%02d %02d:%02d:%02d", d->year, d->month, d->dayOfMonth, d->hour, d->minute, d->second);
-}
 
 
 #endif // RTC_SHARED_C_
